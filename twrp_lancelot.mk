934612d7613067b631d1b3e25312ede5d11a9e39
Micloudtest <145197598+Micloudtest@users.noreply.github.com>
2024-02-23 18:59:06 +0800

    Update and rename omni_lancelot.mk to twrp_lancelot.mk

---
+++ twrp_lancelot.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit from rosemary device
+$(call inherit-product, device/xiaomi/lancelot/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := lancelot
+PRODUCT_NAME := twrp_lancelot
+PRODUCT_BRAND := xiaomi
+PRODUCT_MODEL := Redmi 9
+PRODUCT_MANUFACTURER := xiaomi
